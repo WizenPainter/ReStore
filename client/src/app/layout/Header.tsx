793e816803400fdfb010ac05615352e905a17a2d
Wizen <guzmanpjaime@gmail.com>
2023-09-25 10:46:21 -0600

    End of Section 4

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,26 +1,91 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'about', path: '/about'},
+    {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'register', path: '/register'}
+]
 
 interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
 
+const navStyles = {
+    color: 'inherit', 
+    typography: 'h6',
+    textDecoration: 'none',
+    '&:hover': {
+    color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
+
 export default function Header({ darkMode, handleThemeChange }: Props) {
     return (
         <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Re-Store
-                </Typography>
-                <Switch
-                    color="default"
-                    inputProps={{ 'aria-label': 'checkbox with default color' }}
-                    checked={darkMode}
-                    onChange={() => { 
-                        handleThemeChange()
-                    }
-                    }
-                />
+            <Toolbar sx={{display: 'felx', justifyContent: 'space-between', alignItems: 'center'}}>
+
+                <Box display='flex' alignItems='center'>
+                    <Typography variant="h6" 
+                    component={NavLink} 
+                    sx={navStyles} 
+                    to="/"
+                    >
+                        Re-Store
+                    </Typography>
+                    <Switch
+                        color="default"
+                        inputProps={{ 'aria-label': 'checkbox with default color' }}
+                        checked={darkMode}
+                        onChange={() => { 
+                            handleThemeChange()
+                        }
+                        }
+                    />
+                </Box>
+                
+                <List sx={{ display: 'flex', flexDirection: 'row' }}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem 
+                        component={NavLink} 
+                        to={path} 
+                        key={path} 
+                        sx={navStyles}>
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+                
+                <Box display='flex' alignItems='center'>
+                    <IconButton size='large' edge='start' color='inherit' sx={{mr: 2}}>
+                        <Badge badgeContent='4' color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+                    <List sx={{ display: 'flex', flexDirection: 'row' }}>
+                        {rightLinks.map(({title, path}) => (
+                            <ListItem 
+                            component={NavLink} 
+                            to={path} 
+                            key={path} 
+                            sx={navStyles}>
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                
             </Toolbar>
         </AppBar>
     )
